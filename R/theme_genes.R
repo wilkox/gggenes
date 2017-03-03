659ecb163c32830c7db17fee37b7b87e2d8559fd
David Wilkins <david@wilkox.org>
2017-03-03 17:41:53 +1100

    Remove facet strips in theme_genes

--- R/theme_genes.R
+++ R/theme_genes.R
@@ -3,6 +3,11 @@
 #'
 #' @description
 #' A ggplot2 theme optimised for drawing gene maps.
+#'
+#' This theme removes strip text (the text that labels facets when you use
+#' \code{facet_wrap} or \code{facet_grid}. This is to allow for easy
+#' drawing of molecules on different x-scales by setting the y aesthetic to the
+#' molecule, then faceting with `facet_grid( ~ molecule, scales = "free")`.
 theme_genes <- function(...) {
     theme_grey(...) %+replace% theme(
       panel.background = element_blank(),
@@ -12,6 +17,8 @@ theme_genes <- function(...) {
       panel.grid.major.x = element_blank(),
       axis.ticks.y = element_blank(),
       axis.line.x = element_line(colour = "grey20", size = 0.5),
-      axis.ticks.x = element_line(colour = "grey20", size = 0.5)
+      axis.ticks.x = element_line(colour = "grey20", size = 0.5),
+      strip.text = element_blank(),
+      strip.background = element_blank()
     )
 }
