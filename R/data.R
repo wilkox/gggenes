c6a4c42f72e12e440714f311923408f27b6cb89b
David Wilkins <david@wilkox.org>
2017-08-24 20:27:21 +1000

    Document package and dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' A set of example genes.
+#'
+#' Genes for example purposes only.
+#'
+#' @format A data frame with 118 rows and four variables:
+#' \describe{
+#'   \item{molecule}{the genome}
+#'   \item{start}{the start position of the gene}
+#'   \item{end}{the end position of the gene}
+#'   \item{gene}{the name of the gene}
+#' }
+"example_genes"
