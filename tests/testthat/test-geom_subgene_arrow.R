08329a2dbfeaa8a6e08da08a51de8c50582e8952
Marco Chiapello <chiapello.m@gmail.com>
2020-04-16 09:58:58 +0200

    Removed condition for subgenes - try to solve #21

--- tests/testthat/test-geom_subgene_arrow.R
+++ tests/testthat/test-geom_subgene_arrow.R
@@ -25,22 +25,3 @@ test_that("a simple geom_subgene_arrow plot is drawn without errors", {
   } )
 })
 
-test_that("boundary breaking subgenes are caught", {
-  ## only 1 valid subgene
-  genes <- data.frame(
-    gene = c("cds1", "cd2", "cd3", "cds4"),
-    start = c(2000, 2000, 2050, 2050),
-    end = c(1000, 1000, 3000, 3000),
-    subgenestart =  c(1200, 1000, 2000, 2800),
-    subgeneend = c(1000, 600, 2200, 3200))
-
-    expect_warning(
-      print(plt <- ggplot(genes, aes(xmin = start, xmax = end, y = "strand")) +
-                        geom_gene_arrow() +
-                        geom_subgene_arrow(aes(xsubmin = subgenestart,
-                                       xsubmax = subgeneend), fill = "blue")),
-      regex="Subgene 2.*Subgene 3.*Subgene 4"
-    )
-
-    expect_is(plt, "ggplot")
-})
